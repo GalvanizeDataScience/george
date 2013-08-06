477637f0bcf662841811b928e8ae087f3f7c2f42
Thomas Levine <occurrence@thomaslevine.com>
2013-08-06 16:46:49 -0700

    tests run in /tmp directories

--- tests/test_check.py
+++ tests/test_check.py
@@ -1,17 +1,27 @@
 import os
-from george import check
+import shutil
 import nose.tools as n
+import uuid
+
+from george import check
 
 ROOT = os.getcwd()
 
+
+
 def setup():
+    # Make a temporary directory
+    tmp = os.path.join('/', 'tmp', 'george-' + unicode(uuid.uuid1()))
+    os.mkdir(tmp)
+    os.chdir(tmp)
+
+def teardown():
+    tmp = os.getcwd()
     os.chdir(ROOT)
 
 def test_test_has_data_submodule():
-    os.chdir(os.path.join('fixtures', 'test_has_data_submodule', 'pass'))
-    check.test_has_data_submodule()
-
-@n.nottest
-def test_test_has_data_submodule():
-    os.chdir(os.path.join('fixtures', 'test_has_data_submodule', 'fail'))
     n.assert_raises(AssertionError, check.test_has_data_submodule)
+    open('.gitmodules', 'w').write('foo')
+    os.mkdir('data')
+    open(os.path.join('data', '.git'), 'w').write('bar')
+    check.test_has_data_submodule()
