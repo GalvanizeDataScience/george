43100969f041c305b07dbb5601ca0278fa768f4f
Thomas Levine <occurrence@thomaslevine.com>
2013-08-06 17:13:53 -0700

    write the readme

--- george/init.py
+++ george/init.py
@@ -6,4 +6,7 @@ def directories():
         open(os.path.join(directory, '.gitignore'), 'w').write('')
 
 def readme():
-    raise NotImplementedError
+    fp = open('readme.md', 'w')
+    for section in ['Overview', 'References', 'Goals', 'Assignment', 'Extra Credit', 'Glossary']:
+        fp.write('\n## %s\n' % section)
+    fp.close()
