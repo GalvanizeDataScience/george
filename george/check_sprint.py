5d1aac5ee3a607650df225d903555c69588ec4de
Thomas Levine <occurrence@thomaslevine.com>
2013-08-09 15:26:10 -0700

    fix python_files_correspond

--- george/check_sprint.py
+++ george/check_sprint.py
@@ -33,10 +33,16 @@ def test_python_init():
 
 def test_python_files_correspond():
     '.py files in ``code`` should correspond with test files in ``test``'
-    n.assert_true(os.path.isdir('code'))
-    code_files = filter(lambda filename: filename.endswith('.py'), os.listdir('code'))
+    if not (os.path.isdir('code') and os.path.isdir('test')):
+        return
+
+    code_files = set(filter(lambda filename: filename.endswith('.py'), os.listdir('code')))
     test_files = filter(lambda filename: filename.startswith('test_') and filename.endswith('.py'), os.listdir('test'))
-    n.assert_set_equal(set([test_file[5:] for test_file in test_files]), set(code_files))
+
+    if '__init__.py' in code_files:
+        code_files.remove('__init__.py')
+
+    n.assert_set_equal(set([test_file[5:] for test_file in test_files]), code_files)
 
 def test_shell_files_correspond():
     '.sh files in ``code`` should correspond with test directories in ``test``'
